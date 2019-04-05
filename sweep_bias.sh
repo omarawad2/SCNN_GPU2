4f8844f9754aaa50a02e2343faa0494f435060ba
Elgammal  Mohamed Adel Attia Elhady <elgamma8@ug60.eecg.toronto.edu>
2019-04-05 10:56:07 -0400

    Adding unified sweeping bias script

---
+++ sweep_bias.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+
+for num in  8 32 16
+    do
+        num2=`expr 1024 / $num / $num`
+        sed -i "s/\#define bias_xDim [[:digit:]]*/\#define bias_xDim $num/" main.cu
+        sed -i "s/\#define bias_yDim [[:digit:]]*/\#define bias_yDim $num/" main.cu
+        sed -i "s/\#define bias_zDim [[:digit:]]*/\#define bias_zDim $num2/" main.cu
+        echo -n "$num, $num, $num2, ">>result_bias.rpt
+        ./compile.sh
+        ./SCNN_GPU bvlc_alexnet | sed -n 's/Total time://p'>>result_bias.rpt
+    done
+
